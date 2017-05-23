2c2f47b444ef8853239621634bdb5088218fb2fe
Maxime Bret <maxime.bret.ext@boursorama.fr>
2017-05-23 19:04:23 +0200

    no message

--- src/components/tweets/tweets-action-creators.service.js
+++ src/components/tweets/tweets-action-creators.service.js
@@ -1,47 +1,46 @@
-(function() {
+(function () {
 
-    const factory = (tweetsService, $q) => {
-        "ngInject";
+  const factory = (tweetsService, $q) => {
+    'ngInject'
 
-        const tweetsReceived = (items) => {
-            return {
-                type: "TWEETS_RECEIVED",
-                items
-            }
-        };
-
-        const tweetsFetching = () => {
-            return {
-                type: "TWEETS_FETCHING"
-            }
-        };
-
-        /**
-         * Fetch tweets (once in a time)
-         */
-        const fetchTweets = () => {
-            return (dispatch, getState) => {
-                if (!getState().tweets.fetching) {
-                    dispatch(tweetsFetching());
-                    return tweetsService.fetchTweets()
-                        .then((tweets) => {
-                            dispatch(tweetsReceived(tweets));
-                        });
-                }
-
-                return $q.resolve();
-            }
-        }
-
-        return {
-            fetchTweets,
-            tweetsFetching,
-            tweetsReceived
-        }
+    const tweetsReceived = (items) => {
+      return {
+        type: 'TWEETS_RECEIVED',
+        items,
+      }
     }
 
+    const tweetsFetching = () => {
+      return {
+        type: 'TWEETS_FETCHING',
+      }
+    }
 
-    angular
-        .module("app.tweets")
-        .factory("tweetsActionCreators", factory);
-})();
+    /**
+     * Fetch tweets (once in a time)
+     */
+    const fetchTweets = () => {
+      return (dispatch, getState) => {
+        if (!getState().tweets.fetching) {
+          dispatch(tweetsFetching())
+          return tweetsService.fetchTweets()
+            .then((tweets) => {
+              dispatch(tweetsReceived(tweets))
+            })
+        }
+
+        return $q.resolve()
+      }
+    }
+
+    return {
+      fetchTweets,
+      tweetsFetching,
+      tweetsReceived,
+    }
+  }
+
+  angular
+    .module('app.tweets')
+    .factory('tweetsActionCreators', factory)
+})()

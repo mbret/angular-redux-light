e7eadb249d9b15b361ea3abddfcd07ea5d24972b
Maxime Bret <maxime.bret.ext@boursorama.fr>
2017-05-23 11:08:42 +0200

    no message

--- src/app.component.js
+++ src/app.component.js
@@ -1,17 +1,25 @@
-(function() {
+(function () {
 
-    class App {
-        constructor() {
+  class App {
+    constructor () {
 
-        }
     }
+  }
 
-    const component = {
-        template: `<ui-view></ui-view>`,
-        controller: App
-    };
+  const component = {
+    template: `
+        <div>
+            <h1>My awesome redux Angular app!</h1>
+            <a ui-sref="app.todos">Go home (sync)</a>
+            <a ui-sref="app.settings">Go settings (sync)</a>
+            <a ui-sref="app.tweets">Go tweets (async)</a>
+            <ui-view></ui-view>
+        </div>
+    `,
+    controller: App,
+  }
 
-    angular
-        .module("app")
-        .component("app", component)
-})();
+  angular
+    .module('app')
+    .component('app', component)
+})()
